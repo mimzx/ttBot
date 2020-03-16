b46495c7364c37e749d57c794523b5146cb79f59
alxegorov <alexey.ig.egorov@gmail.com>
2020-03-16 15:29:10 +0300

    Send 'Hello' to every message

--- bot.py
+++ bot.py
@@ -1,14 +1,27 @@
 import requests
 import os
 import time
+import datetime
 from config import Config
+from termcolor import colored
 
-def get_url(action):
+# Make URL
+def get_url(action, **params):
     API_TOKEN = os.environ.get('API_TOKEN')
     url = Config.API_URL + action + '?access_token=' + API_TOKEN
+    if params:
+        for key, value in params.items():
+            url = url + '&' + key + '=' + value
     return url
 
+# Listener
 while True:
-    r = requests.get(get_url('updates'))
-    print(r.text)
-    time.sleep(0.5)
+    get_updates = requests.get(get_url('updates'))
+    updates = get_updates.json()['updates']
+    print(colored(datetime.datetime.now(), 'red'), colored('updates', 'green'), updates)
+    if (updates):
+        for update in updates:
+            user_id = update['message']['sender']['user_id']
+            post_message = requests.post(get_url('messages', user_id=str(user_id)), data='{"text":"Hello"}')
+            print(colored(datetime.datetime.now(), 'red'), colored('message', 'yellow'), post_message.json()['message'])
+    time.sleep(Config.LISTEN_INTERVAL)
--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 class Config(object):
     API_URL='https://botapi.tamtam.chat/'
+    LISTEN_INTERVAL = 0.5
